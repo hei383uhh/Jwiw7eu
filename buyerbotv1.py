7daed8c62a219341a4631d0e594556c8e80a09b5
hei383uhh <hsu28373hh@outlook.fr>
2026-08-15 10:25:06 +0600

    Update buyerbotv1.py

--- buyerbotv1.py
+++ buyerbotv1.py
@@ -247,7 +247,7 @@ def ibtn(text, **kwargs):
         kwargs.pop("icon_custom_emoji_id", None)
         kwargs.pop("style", None)
         return _InlineKeyboardButton(text=text, **kwargs)
-TOKEN = "8955454814:AAF9dJi46wUi5u0hDgY2rg3wGxP_ONhSg4Y"
+TOKEN = "8955454814:AAH4VcqcC0i5H0zZtCU11dI7r67IElEJX_k"
 ADMIN_ID = 6995426618
 DB=os.getenv("DB_PATH","bot.db")
 FD=Path(os.getenv("FILES_DIR","uploads")); FD.mkdir(parents=True,exist_ok=True)
